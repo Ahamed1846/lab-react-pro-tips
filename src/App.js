9f13759e20543f8709c84e61ba0db75730522e83
unknown <Ahamed1846>
2023-03-06 15:54:21 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Contact from './components/Contact';
-import About from './components/About';
+import Register from './components/Register';
 
 function App() { 
   return (
@@ -12,14 +12,14 @@ function App() {
         <NavLink to="/"><h5>Kalvium❤️</h5></NavLink>
         <div className='right'>
           <NavLink to="/Contact" style={{ display: 'inline-block' }}><h5>Contact</h5></NavLink>
-          <NavLink to="/About" style={{ display: 'inline-block' }}><h5>About</h5></NavLink>
+          <NavLink to="/Register" style={{ display: 'inline-block' }}><h5>Register</h5></NavLink>
         </div>
       </div>
         <div>
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/Contact' element={<Contact />} />
-            <Route path='/About' element={<About />} />
+            <Route path='/Register' element={<Register />} />
           </Routes>
         </div>
       
